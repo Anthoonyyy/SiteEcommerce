06fab17e963961bebcf9086d97375107cb8deb26
Anthoonyyy <Anthony212007@hotmail.com>
2024-01-21 22:40:29 +0100

    Suppression video et ajout d'un slider

---
+++ index.js
@@ -0,0 +1,12 @@
+const slide = ["fashion womanresize.png", "fashion menresize.png"];
+let numero = 0;
+
+function ChangeSlide(sens) {
+    numero = numero + sens;
+    if (numero > slide.length - 1)
+        numero = 0;
+    if (numero < 0)
+        numero = slide.length - 1;
+    document.getElementById("slide").src = "sliders/" + slide[numero];
+}
+setInterval("ChangeSlide(1)", 4000);
